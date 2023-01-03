31f60595c2850b153f3398fed28318cfc507446f
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-01-02 19:57:44 -0500

    models specification

--- polls/models.py
+++ polls/models.py
@@ -1,17 +1,21 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200) # CharField is a field type
-    pub_date = models.DateTimeField('date published') # 'date published' is a human-readable name for the field
+    pub_date = models.DateTimeField(default=timezone.now) # 'date published' is a human-readable name for the field
 
     def __str__(self) -> str:
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # ForeignKey is a relationship
     choice_text = models.CharField(max_length=200) 
     votes = models.IntegerField(default=0) # IntegerField is a field type
-
+    
     def __str__(self) -> str:
-        return self.choice_text
+        return f"Question #{self.pk}" + self.choice_text
+
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Question
+
 
 # Create your views here.
 def index(request):
+    question = Question.get_objects()
+    print("Questions: " + question)
+
     return render(request, 'polls/index.html')
