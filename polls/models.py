013d8e0abd4dca8dbe96d31057ed38932ef132f8
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-01-07 11:32:11 -0500

    detail template with choices

--- polls/models.py
+++ polls/models.py
@@ -12,7 +12,7 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='question_choices') # ForeignKey is a relationship
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # ForeignKey is a relationship
     choice_text = models.CharField(max_length=200) 
     votes = models.IntegerField(default=0) # IntegerField is a field type
     
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 
@@ -10,7 +10,9 @@ def index(request):
     })
 
 def detail(request, question_id):
-    return render(request, 'polls/detail.html', {'question_id': question_id})
+    question = get_object_or_404(Question, pk=question_id)
+    choices = question.choice_set.all()
+    return render(request, 'polls/detail.html', {'question': question, 'choices': choices})
 
 def results(request, question_id):
     return render(request, 'polls/results.html', {'question_id': question_id})
