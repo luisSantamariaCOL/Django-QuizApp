1d9b0ad8abf34f8ba352e21e07be0a442a542f48
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-01-06 21:55:46 -0500

    polls views created

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,14 @@ from .models import Question
 
 # Create your views here.
 def index(request):
-    question = Question.get_objects()
-    print("Questions: " + question)
 
     return render(request, 'polls/index.html')
+
+def detail(request, question_id):
+    return render(request, 'polls/detail.html', {'question_id': question_id})
+
+def results(request, question_id):
+    return render(request, 'polls/results.html', {'question_id': question_id})
+
+def vote(request, question_id):
+    return render(request, 'polls/vote.html', {'question_id': question_id})
